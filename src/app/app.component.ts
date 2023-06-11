bd4997c03636b04282c11ccbeb31d7dc4ba8c527
ChithirakrishnaMv <chithirakrishnamv@gmail.com>
2023-06-11 19:32:09 +0530

    Currency Convector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {CurrencyService} from './service/converter.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Currency-Converter';
+export class AppComponent implements OnInit {
+  public amount:number=1;
+  public currfrom:any;
+  public currto:any;
+  public data:any;
+  public valuefrom:any;
+  public valueto:any;
+  public result:any;
+  public flagfrom:string ='ma'.toLowerCase();
+  public flagto:string = 'us'.toLowerCase();
+to: any;
+curr: any;
+  
+  public constructor(private convertor:CurrencyService){}
+  ngOnInit(): void {
+   
+   this.getcountryfrom()
+   this.getcountryto()
+  }
+  getcountryfrom(){
+    this.convertor.getCountries().subscribe(data=>
+      this.currfrom = Object.entries(data).map((value) => value[0].toUpperCase())
+      )
+  }
+  getcountryto(){
+    this.convertor.getCountries().subscribe(data=>
+      this.currto = Object.entries(data).map((value)=>value[0].toUpperCase())
+      );
+  }
+  currencyconverter(from:any, to:any){
+    this.convertor.convert(from, to).subscribe(data=>{
+    this.data = Object.entries(data).map(x => x[1])  
+    this.valuefrom = Object.entries(this.data[4]).map(x=>x[1])[0]
+    this.valueto  = Object.entries(this.data[4]).map(x=>x[1])[1]
+    this.result = Number((this.amount*(this.valueto/this.valuefrom)).toFixed(2));        
+    console.log(this.result.toFixed(2));
+    
+  }); 
+
+  }
+  getflag(flag?:any,to?:any){
+    this.flagfrom = flag.toString().slice(0, -1).toLowerCase();
+    this.flagto = to.toString().slice(0, -1).toLowerCase();
+    console.log(this.flagfrom);
+
+  }
+  
+  title = 'converter';
 }
---
+++ src/app/service/converter.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class CurrencyService {
+  Baseurl:string = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json';
+  constructor (private http:HttpClient){}
+  getCountries(){
+  return this.http.get(this.Baseurl)
+  }
+  convert(from:any,to:any){
+      return this.http.get(`http://data.fixer.io/api/latest?access_key=8589e210c7837aca1457d193c1650b7d&symbols=${from},${to}&format=1`) 
+  }
+  
+  }
+    
